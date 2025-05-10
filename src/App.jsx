c499672f1b70f96de382ec0561cbd47ba91120c9
aamidou <akimayena26@gmail.com>
2025-05-10 11:03:54 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Card />
-    </>
-  )
+  return <Card />
 }
-
-//https://jsonplaceholder.typicode.com/users
 
 export default App
