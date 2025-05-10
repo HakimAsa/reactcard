9e89cf930b3264cc5d493f07152c4e8cb3e52ced
aamidou <akimayena26@gmail.com>
2025-05-10 11:02:02 +0100

    commit initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+//https://jsonplaceholder.typicode.com/users
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './card.css'
+//https://jsonplaceholder.typicode.com/users
+import avatar from '../assets/avatar.png'
+
+export default function Card() {
+  const [users, setUsers] = React.useState([])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setUsers(data)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <div style={{ height: '100px', color: 'purple' }}></div>
+      <div className="card-container">
+        <div className="ellipse"></div> {/* Add this div for the ellipse */}
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="card"
+          >
+            <img
+              src={avatar}
+              alt={user.name}
+            />
+            <h2>{user.name}</h2>
+            <div className="details">
+              <div className="project">
+                <strong style={{ fontSize: 12 }}>
+                  {user.phone.split('x')[0]}
+                </strong>
+                <p>Phone</p>
+              </div>
+              <div className="prototype">
+                <strong style={{ fontSize: 12 }}>{user.website}</strong>
+                <p>Website</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
